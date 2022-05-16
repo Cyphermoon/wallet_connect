0cbb3b88a62dc87b600e808ac55f7b932165e3ca
Cypher Moon <temperaturecaleb@gmail.com>
2022-05-16 23:55:26 +0100

    Changed the loading indicator

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -1,6 +1,6 @@
 import ReactDom from 'react-dom'
 import Overlay from './Overlay'
-import { MoonLoader } from 'react-spinners';
+import { RingLoader } from 'react-spinners';
 
 const Loading = ({ loadingMsg  }) => {
   return ReactDom.createPortal(
@@ -8,7 +8,7 @@ const Loading = ({ loadingMsg  }) => {
         <div className=" w-11/12 z-50 max-w-xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2 shadow-lg rounded-lg bg-white p-8">
             <div className='flex flex-col h-5/6 justify-evenly items-center'>
                 <p className=' text-gray-800 text-xl sm:text-2xl'> {loadingMsg}</p> 
-                <MoonLoader size={70} color="#549eff" />
+                <RingLoader size={70} color="#549eff" />
             </div>
 
         </div>
